cbaba7187c550aab356cc1801ca1bc70a1d6d126
bahasalien <syamsiralam.id@gmai.com>
2020-12-24 20:10:43 +0800

    Color and color.

--- src/index.js
+++ src/index.js
@@ -5,14 +5,19 @@ function component() {
     const element = document.createElement('div');
 
     // Lodash, now imported by this script
-    const results = ["1","2","3","4","1","2","3","4"];
+    const results = ['Test'];
     const div = document.getElementById('elements');
     element.innerHTML = results.join('<br>');
     element.classList.add(
         'body',
         'button');
-
+        
     return element;
  }
  
+function button() {
+
+
+}
+
  document.body.appendChild(component());
